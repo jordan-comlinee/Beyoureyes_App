4e2a7d0473818594b5dca44ee6fd3d54d96cf3b8
moon0900 <moon09@duksung.ac.kr>
2024-01-09 11:51:39 +0900

    패키지명 변경

--- app/src/main/java/com/dna/beyoureyes/CameraFocusProblemActivity.kt
+++ app/src/main/java/com/dna/beyoureyes/CameraFocusProblemActivity.kt
@@ -14,33 +14,8 @@ import org.opencv.android.OpenCVLoader
 class CameraFocusProblemActivity : AppCompatActivity()  {
 
     // 권한 처리에 필요한 변수
-    val CAMERA_PERMISSION = arrayOf(Manifest.permission.CAMERA)
-    val FLAG_PERM_CAMERA = 98
-    val FLAG_REQ_CAMERA = 101
-    val BLUR_SCORE_THRESH = 40
-    lateinit var currentPhotoPath: String
     private lateinit var binding: ActivityCameraFocusProblemBinding
-
-    @Throws(IOException::class)
-    private fun createImageFile(): File {
-        // Create an image file name
-        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        return File.createTempFile(
-            "JPEG_${timeStamp}_", /* prefix */
-            ".jpg", /* suffix */
-            storageDir /* directory */
-        ).apply {
-            // Save a file: path for use with ACTION_VIEW intents
-            currentPhotoPath = absolutePath
-        }
-    }
-
-    fun rotateImage(source: Bitmap, angle: Float): Bitmap {
-        val matrix = Matrix()
-        matrix.postRotate(angle)
-        return Bitmap.createBitmap(source, 0, 0, source.width, source.height, matrix, true)
-    }
+    private val camera = Camera()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -60,10 +35,8 @@ class CameraFocusProblemActivity : AppCompatActivity()  {
 
         // 카메라
         binding.buttoncamera.setOnClickListener {
-            if(isPermission(CAMERA_PERMISSION)){
-                dispatchTakePictureIntent()
-            } else {
-                ActivityCompat.requestPermissions(this, CAMERA_PERMISSION, FLAG_PERM_CAMERA)
+            while(camera.start(this) == -1){
+                camera.start(this)
             }
         }
 
@@ -81,173 +54,14 @@ class CameraFocusProblemActivity : AppCompatActivity()  {
         binding.textView1.text = spannable
     }
 
-    fun isPermission(permissions:Array<String>) : Boolean {
-
-        for(permission in permissions) {
-            val result = ContextCompat.checkSelfPermission(this, permission)
-            if (result != PackageManager.PERMISSION_GRANTED) {
-                return false
-            }
-        }
-
-        return true
-    }
-
-    private fun dispatchTakePictureIntent() {
-        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
-            // Ensure that there's a camera activity to handle the intent
-            takePictureIntent.resolveActivity(packageManager)?.also {
-                // Create the File where the photo should go
-                val photoFile: File? = try {
-                    createImageFile()
-                } catch (ex: IOException) {
-                    // Error occurred while creating the File
-                    null
-                }
-                // Continue only if the File was successfully created
-                photoFile?.also {
-                    val photoURI: Uri = FileProvider.getUriForFile(
-                        this,
-                        "com.dna.beyoureyes.fileprovider",
-                        it
-                    )
-                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
-                    startActivityForResult(takePictureIntent, FLAG_REQ_CAMERA)
-                }
-            }
-        }
-    }
-
-
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if(resultCode == Activity.RESULT_OK){
             when(requestCode) {
-                FLAG_REQ_CAMERA -> {
-
-                    val f = File(currentPhotoPath)
-                    val uri = Uri.fromFile(f)
-
-                    var bitmap: Bitmap? = null
-
-                    if(Build.VERSION.SDK_INT < 28) {
-                        bitmap = MediaStore.Images.Media.getBitmap(
-                            this.contentResolver,
-                            uri
-                        )
-                    } else {
-                        val source = ImageDecoder.createSource(this.contentResolver, uri)
-                        bitmap = ImageDecoder.decodeBitmap(source) { decoder, _, _ ->
-                            decoder.setTargetSampleSize(1) // shrinking by
-                            decoder.isMutableRequired = true // this resolve the hardware type of bitmap problem
-                        }
-                    }
-
-
-
-                    if (bitmap != null) {
-
-                        // rotation check
-                        val ei = ExifInterface(currentPhotoPath)
-                        val orientation: Int = ei.getAttributeInt(
-                            ExifInterface.TAG_ORIENTATION,
-                            ExifInterface.ORIENTATION_UNDEFINED
-                        )
-
-                        bitmap = when (orientation) {
-                            ExifInterface.ORIENTATION_ROTATE_90 -> bitmap
-                            ExifInterface.ORIENTATION_ROTATE_180 -> rotateImage(bitmap, -90f)
-                            ExifInterface.ORIENTATION_ROTATE_270 -> rotateImage(bitmap, 180f)
-                            ExifInterface.ORIENTATION_NORMAL -> rotateImage(bitmap, 90f)
-                            else -> bitmap
-                        }
-
-                        // blur check
-                        val blur_score = getBlurScore(bitmap)
-                        if ( blur_score < BLUR_SCORE_THRESH) {
-                            // 재촬영 요구
-
-                        }else{
-
-                            val intent = Intent(this, LoadingActivity::class.java)
-                            val output = histogramEqual(bitmap)
-
-                            // 파일로 비트맵 저장
-                            val stream = FileOutputStream(f)
-                            output.compress(Bitmap.CompressFormat.JPEG, 100, stream)
-                            stream.close()
-
-                            // 파일 경로를 인텐트에 추가
-                            intent.putExtra("bitmapPath", f.absolutePath)
-                            startActivity(intent)
-                        }
-                    }else{
-                        Toast.makeText(this, "사진 촬영에 실패하였습니다.", Toast.LENGTH_LONG).show()
-                    }
+                Camera.FLAG_REQ_CAMERA -> {
+                    camera.processPhoto(this)
                 }
             }
         }
     }
-
-    private fun getBlurScore(bitmap: Bitmap): Double {
-        val outputBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true)
-        val src = Mat()
-        Utils.bitmapToMat(outputBitmap, src)
-        // to grayscale
-        val graySrc = Mat()
-        Imgproc.cvtColor(src, graySrc, Imgproc.COLOR_BGR2GRAY)
-        val laplacian = Mat()
-        Imgproc.Laplacian(graySrc, laplacian, CvType.CV_64F)
-
-        // Calculate the variance of the Laplacian
-        val variance = Mat()
-        Core.multiply(laplacian, laplacian, variance)
-        val mean = Core.mean(variance)
-        return mean.`val`[0]
-    }
-
-    fun histogramEqual(bitmap: Bitmap): Bitmap {
-        // val output = Mat()
-        val outputBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true)
-        val image = Mat()
-        Utils.bitmapToMat(outputBitmap, image)
-
-        // Color image
-        if (image.channels() == 4) {
-            val ycrcbArray = Mat()
-            Imgproc.cvtColor(image, ycrcbArray, Imgproc.COLOR_RGB2YCrCb)
-
-            val channels = ArrayList<Mat>(3)
-            Core.split(ycrcbArray, channels)
-
-            Imgproc.equalizeHist(channels[0], channels[0])
-
-            Core.merge(channels, ycrcbArray)
-            Imgproc.cvtColor(ycrcbArray, image, Imgproc.COLOR_YCrCb2RGB)
-        }
-        // Gray image
-        else {
-            Imgproc.equalizeHist(image, image)
-        }
-
-        Utils.matToBitmap(image, outputBitmap)
-
-        return outputBitmap
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-
-        if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
-        }
-
-
-    }
-
-
 }
